3bf3f4f7a962f3147ec9d70add028c65006e1a01
CharlesIV <cdperkinsiv@gmail.com>
2021-06-20 22:28:00 -0400

    Added/Updated license headers

--- src/com/reactnebula/simplejsonutil/ObjectFactory.java
+++ src/com/reactnebula/simplejsonutil/ObjectFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Charles
+ * Copyright (C) 2021 Charles
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
